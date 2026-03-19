b9aef439910585631a37eb15fc3562316c2d4d51
Manish Mathur <madiv@Manishs-MacBook-Pro.local>
2026-03-20 00:36:13 +0530

    feat(cpp): add initial reverseWords function skeleton C✚✚🚧

---
+++ source/cpp/solution.hpp
@@ -0,0 +1,14 @@
+#ifndef SOLUTION_HPP
+#define SOLUTION_HPP
+
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        return "";
+    }
+};
+
+#endif
