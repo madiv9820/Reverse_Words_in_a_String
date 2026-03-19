f52d2372541dc4d724dd6f59ed9416fd898739ce
Manish Mathur <madiv@Manishs-MacBook-Pro.local>
2026-03-20 01:01:06 +0530

    feat(c): add initial reverseWords function skeleton ⚙️🚧

--- source/c/solution.h
+++ source/c/solution.h
@@ -1,8 +1,12 @@
 #ifndef SOLUTION_H
 #define SOLUTION_H
 
+#include <stdlib.h>
+
 char* reverseWords(char* s) {
-    return "";
+    char* result = (char*) malloc(1 * sizeof(char));
+    result[0] = '\0';
+    return result;
 }
 
 #endif
